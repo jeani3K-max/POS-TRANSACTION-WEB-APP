ab2a08d32bb8c55a72c070fe3c91986b5588a424
jeffrey nosakhare <jayjeff234@gmail.com>
2026-07-31 00:05:54 +0100

    Use environment PORT for deployment in POS TRANSACTION WEB APP

--- server/index.js
+++ server/index.js
@@ -6,8 +6,7 @@ const authRoutes = require("./routes/auth");
 
 
 const app = express();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
